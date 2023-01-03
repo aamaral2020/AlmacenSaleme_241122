362cd6431089a2fc0bb918ef5e84c09503edc85c
Gabriela Rivero <regaby@gmail.com>
2023-01-03 18:01:19 -0300

    [FIX] remove line: remove discount or card charge

--- pos_payment_amount/static/src/js/models.js
+++ pos_payment_amount/static/src/js/models.js
@@ -5,6 +5,8 @@ odoo.define('pos_payment_amount.models', function (require) {
     var utils = require('web.utils');
     var round_pr = utils.round_precision;
 
+    var posorder_super = models.Order.prototype;
+
     models.Order = models.Order.extend({
 
         get_acum: function(paymentline) {
@@ -20,6 +22,24 @@ odoo.define('pos_payment_amount.models', function (require) {
             }
             return round_pr(acum, this.pos.currency.rounding);
         },
+
+        remove_paymentline: function(line) {
+            let order = this.pos.get_order();
+            var lines = order.get_orderlines();
+            if (line['payment_method']['type']=='cash') {
+                var product  = this.pos.db.get_product_by_id(this.pos.config.saleme_discount_product_id[0]);
+            }
+            else {
+                var product  = this.pos.db.get_product_by_id(line['payment_method']['instalment_product_id'][0]);
+            }
+            // Remove existing discounts
+            for (const line of lines) {
+                if (line.get_product() === product) {
+                    order.remove_orderline(line);
+                }
+            }
+            posorder_super.remove_paymentline.apply(this, arguments);
+        }
 
     });
 
